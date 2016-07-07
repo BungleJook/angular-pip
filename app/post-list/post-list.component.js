6e82e228cbef893428aac9e7f1874a0714943057
Nick Edwards <BungleJook@users.noreply.github.com>
2016-07-07 18:33:44 +0100

    Add posts.json; update posts component to use posts.json

--- app/post-list/post-list.component.js
+++ app/post-list/post-list.component.js
@@ -2,18 +2,13 @@ angular.
     module('postList').
     component('postList', {
         templateUrl: 'post-list/post-list.template.html',
-        controller: function PostListController() {
-            this.posts = [
-                {
-                    title: 'Article One',
-                    text: 'This is an example for Article One.'
-                }, {
-                    title: 'Article Two',
-                    text: 'This is an example for Article Two.'
-                }, {
-                    title: 'Article Three',
-                    text: 'This is an example for Article Three.'
-                }
-            ];
-        }
+        controller: ['$http',
+            function PostListController($http) {
+                var self = this;
+
+                $http.get('posts/posts.json').then(function(response) {
+                    self.posts = response.data;
+                })
+            }
+        ]
     });
